ae2d806160f8659d481925b304ff7b70a05e4e17
JVDesignBavaria <jvdesign@mail.de>
2024-10-20 16:28:28 +0200

    Gradia Beta 1.0.8
    Added analytics logging

--- gradia/script.js
+++ gradia/script.js
@@ -1,4 +1,4 @@
-const buildVersion = 'Beta 1.0.7'
+const buildVersion = 'Beta 1.0.8'
 
 let subjects = [];
 
@@ -1136,6 +1136,54 @@ function handleUpdate(updateData) {
 }
 
 
+
+//----- SENDS LOG DATA -----//
+
+async function sendLogData(logType) {
+    const versionData = {
+        app_name: "gradia",
+        version: buildVersion,
+        new_users: 0,
+        standalone_apps: 0,
+        updated_apps: 0
+    };
+
+    switch(logType) {
+        case 'new_user':
+            versionData.new_users ++;
+            break;
+        case 'standalone':
+            versionData.standalone_apps ++;
+            break;
+        case 'updated':
+            versionData.updated_apps ++;
+            break;
+        default:
+            return;
+    }
+
+    try {
+        const response = await fetch('https://logs.jvdesign.workers.dev', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(versionData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        console.log('Response from worker:', result);
+    } 
+    catch (error) {
+        console.error('Failed to send version data:', error);
+    }
+}
+
+
 //----- CALLED ON EVERY LOAD -----//
 
 window.onload = () => {
@@ -1364,6 +1412,8 @@ function init() {
             de:`Gradia speichert deine Daten lokal im Browser. <br> Um einen Datenverlust im Falle der Löschung der Browser-Daten zu verhindern, solltest du deine Daten regelmäßig in den Einstellungen herunterladen.`, 
             en:`Gradia saves your data locally in your browser. <br> To prevent data loss in case of a deletion of your browser's data, you should download your data from time to time in the settings.`
         }));
+        sendLogData('new_user');
+        if(navigator.standalone) sendLogData('standalone');
     }
     showDownloadMessage();
 
--- gradia/sw.js
+++ gradia/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Beta 1.0.7';
+const CACHE_NAME = 'Beta 1.0.8';
 const INFO = {
     get description() {
         return {de:`Dieses Update enthält Fehlerbehebungen${this.features.length < 1 ? `.`:` und führt diese neuen Features ein:`}`, en:`This update provides bug fixes${this.features.length < 1 ? `.`:` and introduces these new features:`}`}
@@ -29,6 +29,7 @@ self.addEventListener('install', (event) => {
 })
 
 self.addEventListener('activate', (event) => {
+    sendLogData('updated');
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
@@ -63,3 +64,52 @@ async function cacheFirst(request) {
 self.addEventListener('fetch', (event) => {
     event.respondWith(cacheFirst(event.request));
 })
+
+
+
+
+//----- SENDS LOG DATA -----//
+
+async function sendLogData(logType) {
+    const versionData = {
+        app_name: "gradia",
+        version: buildVersion,
+        new_users: 0,
+        standalone_apps: 0,
+        updated_apps: 0
+    };
+
+    switch(logType) {
+        case 'new_user':
+            versionData.new_users ++;
+            break;
+        case 'standalone':
+            versionData.standalone_apps ++;
+            break;
+        case 'updated':
+            versionData.updated_apps ++;
+            break;
+        default:
+            return;
+    }
+
+    try {
+        const response = await fetch('https://logs.jvdesign.workers.dev', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(versionData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        console.log('Response from worker:', result);
+    } 
+    catch (error) {
+        console.error('Failed to send version data:', error);
+    }
+}
